196cd0bed08dadf4752fbb54f514916db536c3d3
Jesse Galloway <JessGalloway23@gmail.com>
2023-03-17 00:28:58 -0500

    Added contact view model and logic to controller, added credentials to appsettings, styled view

---
+++ PortFo/Models/ContactViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortFo.Models
+{
+    public class ContactViewModel
+    {
+
+        [Required(ErrorMessage = "* Name Required")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage ="* Email Required")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage ="* Please Provide A Subject Entry")]
+        public string Subject { get; set; }
+
+
+        [Required(ErrorMessage = "*Message is required")]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+
+
+    }
+}
